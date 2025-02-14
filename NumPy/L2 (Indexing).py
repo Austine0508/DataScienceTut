2eb3bbf55d5f70b54e0844485879d99e2c174eb7
Austine Roy <96955405+Austine0508@users.noreply.github.com>
2025-02-14 18:10:58 +0530

    Add files via upload

---
+++ NumPy/L2 (Indexing).py
@@ -0,0 +1,21 @@
+import numpy as np
+a1=np.array([10,20,30])
+
+print("1-d Indexing:")
+print("Normal Indexing: ",a1[1]);
+print("Negative Indexing: ",a1[-1]);
+
+a2=np.array([[1,2,3],[4,5,6]])
+
+print("\n2-d Indexing:");
+print(a2[1,2]) # prints 2nd row 3rd element
+
+print("\nSlicing:")
+print("1-dimensional slicing: ",a2[1:4])
+print("2-dimensional slicing: ",a2[:,1]) # prints second element of every row
+
+print("\nAdvanced Indexing (uses conditions and other arrays)");
+indices=np.array([0,1])
+print(a1[indices])
+cond=a1>0
+print(a1[cond])
