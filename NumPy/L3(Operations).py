136c03f75d7fb4862cb75e235ac24f5c01300dcf
Austine Roy <96955405+Austine0508@users.noreply.github.com>
2025-02-16 15:22:40 +0530

    Update L3(Operations).py

--- NumPy/L3(Operations).py
+++ NumPy/L3(Operations).py
@@ -12,4 +12,10 @@ print("\nUnary Operation: \n")
 print(np.absolute(c))
 
 print("\nBinary Operation: \n")
-print(np.add(a,b))
+print(np.add(a,b),"\n")
+
+a=np.array([0,np.pi/2,np.pi])
+print("Sines: ",np.sin(a))
+
+b=np.array([0,1,2,4])
+print("Square Roots: ",np.sqrt(b))
