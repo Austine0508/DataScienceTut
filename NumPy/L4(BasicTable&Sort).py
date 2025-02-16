6566c080289dec30e022f03f6a6abe38905bfff7
Austine Roy <96955405+Austine0508@users.noreply.github.com>
2025-02-16 15:39:15 +0530

    Update L4(BasicTable&Sort).py

--- NumPy/L4(BasicTable&Sort).py
+++ NumPy/L4(BasicTable&Sort).py
@@ -1,13 +1,11 @@
 import numpy as np
 
-dtypes=[('name','S10'),('grad_yr',int),('cgpa',float)]
-
-values=[('Delno',2027,7.0),('Austine',2027,8.5),('Elvin',2028,6.6)]
-
-arr=np.array(values,dtype=dtypes)
+dtypes=[('name','S10'),('grad_yr',int),('cgpa',float)] # Table Headings
+values=[('Delno',2027,7.0),('Austine',2027,8.5),('Elvin',2028,6.6)] # Table Values
+arr=np.array(values,dtype=dtypes) # Table Creation
 
 print("Table:\n",arr)
 
-print("\nSorting by Name:\n",np.sort(arr,order='name'))
+print("\nSorting by Name:\n",np.sort(arr,order='name')) # Sorting by Name
 
-print("\nSorting by Grad Yr and CGPA: \n",np.sort(arr,order=['grad_yr','cgpa']))
+print("\nSorting by Grad Yr and CGPA: \n",np.sort(arr,order=['grad_yr','cgpa'])) # Sorting by Grad Yr and CGPA
