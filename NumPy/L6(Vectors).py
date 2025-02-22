42e1498662475232ab2de7bc9c949c7d729b4b8a
Austine Roy <96955405+Austine0508@users.noreply.github.com>
2025-02-22 13:11:21 +0530

    Update L6(Vectors).py

--- NumPy/L6(Vectors).py
+++ NumPy/L6(Vectors).py
@@ -1,8 +1,10 @@
+import numpy as np
+
 # Vectors are 1-D arrays
 # They can be Horizontal or Verticcal
 
 v1=np.array([1,2,3]) # Horizontal Vector
-v2=np.array([1],[2],[3]) # Vertical Vector
+v2=np.array([[1],[2],[3]]) # Vertical Vector
 
 print("Horizontal Vector: \n",v1)
 print("\nVertical Vector: \n",v2)
