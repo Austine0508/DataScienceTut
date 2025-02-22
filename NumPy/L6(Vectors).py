22459fcffac794bb6a0e062d405e6423f595cdf9
Austine Roy <96955405+Austine0508@users.noreply.github.com>
2025-02-22 13:09:27 +0530

    Update L6(Vectors).py

--- NumPy/L6(Vectors).py
+++ NumPy/L6(Vectors).py
@@ -4,3 +4,7 @@
 v1=np.array([1,2,3]) # Horizontal Vector
 v2=np.array([1],[2],[3]) # Vertical Vector
 
+print("Horizontal Vector: \n",v1)
+print("\nVertical Vector: \n",v2)
+
+# All basic operations can be performed on vertors (add, subtract, multiply, divide)
