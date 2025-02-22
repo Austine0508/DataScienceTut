b5efbcc339c0b33c411386fafadbde1a475c3c87
Austine Roy <96955405+Austine0508@users.noreply.github.com>
2025-02-22 13:14:49 +0530

    Update L6(Vectors).py

--- NumPy/L6(Vectors).py
+++ NumPy/L6(Vectors).py
@@ -10,3 +10,11 @@ print("Horizontal Vector: \n",v1)
 print("\nVertical Vector: \n",v2)
 
 # All basic operations can be performed on vertors (add, subtract, multiply, divide)
+
+# dot product can also be accomplished
+
+print("\nDot Product: ",v1.dot(v2))
+
+# scalar product can also be obtained
+
+print("\n Scalar Product: \n",v1*5,"\n\n",v2*5)
