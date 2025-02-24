c668eb04566bad4872b691dc36ca1871e309fa8f
Austine Roy <96955405+Austine0508@users.noreply.github.com>
2025-02-24 22:01:29 +0530

    Update L7(Stacking).py

--- NumPy/L7(Stacking).py
+++ NumPy/L7(Stacking).py
@@ -3,8 +3,8 @@ import numpy as np
 a1=np.array([[1,2,3],[4,5,6]]) 
 a2=np.array([[7,8,9],[10,11,12]]) 
 
-h_arr=np.hstack(a1,a2)
+h_arr=np.hstack((a1,a2))
 print("Horizontal Stacking:\n",h_arr)
 
-v_arr=np.vstcak(a1,a2)
+v_arr=np.vstcak((a1,a2))
 print("Vertical Stacking:\n",v_arr)
