2e8a3358c6761a909a1d0fa43dd9d181df83cf52
Austine Roy <96955405+Austine0508@users.noreply.github.com>
2025-03-06 12:05:57 +0530

    Create L8(STD&AMD).py

---
+++ NumPy/L8(STD&AMD).py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a=np.array([1,2,3,8,9,10])
+
+# Calculate AMD (Absolute Mean Deviation)
+print("Array: ",a)
+d=np.mean(a)
+print("Mean: ",d)
+deviation=a-d
+print("Deviation: ",deviation)
+abs_deviation=abs(deviation)
+print("Absolute Deviation: ",abs_deviation)
+amd=np.sum(abs_deviation)/len(a)
+print("\nAbsolute Mean Deviation: ",amd)
+
+# Calculate STD (Standard Deviation)
+print("\nStandard Deviation: ",np.std(a))
