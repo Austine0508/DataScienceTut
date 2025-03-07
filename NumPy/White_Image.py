a5c8972b6cab9304751d99f1a6b2d83702bf8278
Austine Roy <96955405+Austine0508@users.noreply.github.com>
2025-03-07 10:49:19 +0530

    Create White_Image.py

---
+++ NumPy/White_Image.py
@@ -0,0 +1,6 @@
+import cv2
+import numpy as np
+
+array=np.zeroes([500,500,3]) # Creates an array with 500 matrices of 500 rows and 3 columns with 0 as values
+# In our work here, above line indicates, 500x500 pixels and 3 refers to the color channel RGB (red, green, blue -3)
+
