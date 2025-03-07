0fcb1d41bbfbff7cc675171251379380afba1e2f
Austine Roy <96955405+Austine0508@users.noreply.github.com>
2025-03-07 10:53:12 +0530

    Update White_Image.py

--- NumPy/White_Image.py
+++ NumPy/White_Image.py
@@ -3,6 +3,7 @@ import numpy as np
 
 array=np.zeros([500,500,3],dtype=np.uint8) # Creates an array with 500 matrices of 500 rows and 3 columns with 0 as values
 # In our work here, above line indicates, 500x500 pixels and 3 refers to the color channel RGB (red, green, blue -3)
+# dtype is optional. It specifies the type of data
 
 array[:,:]=[255,255,255] # This lines changes all values to 255
 # 255,255,255 is rgb value of white
