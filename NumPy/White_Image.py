9ae177c513401f0afd733a7f72a3a60717ef0c3b
Austine Roy <96955405+Austine0508@users.noreply.github.com>
2025-03-07 10:52:46 +0530

    Update White_Image.py

--- NumPy/White_Image.py
+++ NumPy/White_Image.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-array=np.zeroes([500,500,3]) # Creates an array with 500 matrices of 500 rows and 3 columns with 0 as values
+array=np.zeros([500,500,3],dtype=np.uint8) # Creates an array with 500 matrices of 500 rows and 3 columns with 0 as values
 # In our work here, above line indicates, 500x500 pixels and 3 refers to the color channel RGB (red, green, blue -3)
 
 array[:,:]=[255,255,255] # This lines changes all values to 255
