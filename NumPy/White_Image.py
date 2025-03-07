5b222fff6688174709b7b2c46db68bc32c644639
Austine Roy <96955405+Austine0508@users.noreply.github.com>
2025-03-07 10:51:34 +0530

    Update White_Image.py

--- NumPy/White_Image.py
+++ NumPy/White_Image.py
@@ -4,3 +4,8 @@ import numpy as np
 array=np.zeroes([500,500,3]) # Creates an array with 500 matrices of 500 rows and 3 columns with 0 as values
 # In our work here, above line indicates, 500x500 pixels and 3 refers to the color channel RGB (red, green, blue -3)
 
+array[:,:]=[255,255,255] # This lines changes all values to 255
+# 255,255,255 is rgb value of white
+
+cv2.imshow("image",array) # Prints our white image
+# "image" is name of window
