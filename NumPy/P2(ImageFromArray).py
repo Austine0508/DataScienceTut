68491c122933dbaff940129a478346f8200396ed
Austine Roy <96955405+Austine0508@users.noreply.github.com>
2025-03-08 11:24:35 +0530

    Update P2(ImageFromArray).py

--- NumPy/P2(ImageFromArray).py
+++ NumPy/P2(ImageFromArray).py
@@ -1,9 +1,17 @@
 import numpy as np
 from PIL import Image as im
 
+# There are 1024*720=737280 pixels. arange them in a line first
 arr=np.arange(737280,dtype=np.uint8)
 print("1\n",arr)
+
+# Resize the array to form a matrix that can be converted into greyscale image
+# Therefore, reshape to 1024x720
 arr=np.reshape(arr,(1024,720))
 print("2\n",arr)
+
 data=im.fromarray(arr)
+# Above line convert arrays into PIL objects
+
 data.save('mypic.png')
+# Above line saves PIL objects in desired format
